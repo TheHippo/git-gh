1d30300e177a526b297a8dcab9417ad478a7f7a5
Philipp Klose <me@thehippo.de>
2014-08-24 21:38:10 +0200

    auth works

--- auth.go
+++ auth.go
@@ -2,20 +2,60 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"os"
-	"time"
 
 	"github.com/google/go-github/github"
 )
 
 const (
-	tokenEnv = "GIT_GH_TOKEN"
+	tokenEnv       = "GIT_GH_TOKEN"
+	githubPassword = "x-oauth-basic"
 )
 
 var authToken string
 
+type transport struct {
+	token     string
+	Transport http.RoundTripper
+}
+
+func cloneRequest(req *http.Request) *http.Request {
+	// shallow copy of the struct
+	clone := new(http.Request)
+	*clone = *req
+	// deep copy of the Header
+	clone.Header = make(http.Header)
+	for k, s := range req.Header {
+		clone.Header[k] = s
+	}
+	return clone
+}
+
+func (t *transport) transport() http.RoundTripper {
+	if t.Transport != nil {
+		return t.Transport
+	}
+	return http.DefaultTransport
+}
+
+func (t *transport) RoundTrip(req *http.Request) (*http.Response, error) {
+	req = cloneRequest(req)
+	req.SetBasicAuth(t.token, githubPassword)
+	return t.transport().RoundTrip(req)
+}
+
+func createHTTPClient(token string) *http.Client {
+	return &http.Client{
+		Transport: &transport{
+			token: token,
+		},
+	}
+}
+
 func init() {
-	authToken := os.Getenv(tokenEnv)
+	// TODO: fetch this from a safer location
+	authToken = os.Getenv(tokenEnv)
 	if len(authToken) == 0 {
 		fmt.Println("Could find auth token in enviroment")
 		os.Exit(1)
@@ -23,5 +63,5 @@ func init() {
 }
 
 func getClient() github.Client {
-	return *github.NewClient()
+	return *github.NewClient(createHTTPClient(authToken))
 }
